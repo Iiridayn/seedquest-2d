a29d90e08011eebe55d6f7ecd29b54c02f7eeb69
Iiridayn <iiridayn@gmail.com>
2020-09-04 15:57:29 -0600

    Fake Uppercase, Bangers font for objective circle

--- components/objective.inc.php
+++ components/objective.inc.php
@@ -1,3 +1,6 @@
+<?php function ucSpanWrap($str) {
+	return preg_replace('/([[:upper:]])/', '<span>\1</span>', $str);
+} ?>
 <figure id="objective">
 <?php
 	$i = min(count($_SESSION['choices']), ($_SESSION['world'] + 1) * ITEMS - 1);
@@ -8,7 +11,7 @@
 ?>
 	<img src="<?= $baseUrl ?>img/scenes/<?= $filename ?>" alt="" onerror="this.src='<?= $baseUrl ?>placeholder.php?w=200&txt=<?= $filename ?>';this.onerror=''" />
 	<figcaption>
-		<h2><?= $item['name'] ?></h2>
-		<p><?= $item['options'][$_SESSION['seed']['choices'][$i][1]] ?></p>
+		<h2><?= ucSpanWrap($item['name']) ?></h2>
+		<p><?= ucSpanWrap($item['options'][$_SESSION['seed']['choices'][$i][1]]) ?></p>
 	</figcaption>
 </figure>
