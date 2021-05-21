0d0c459827b53adb4142bf7268679c9bcd1b5b6f
Iiridayn <iiridayn@gmail.com>
2021-05-20 19:28:23 -0600

    Adust variable name

--- components/item-list.inc.php
+++ components/item-list.inc.php
@@ -2,12 +2,12 @@
 	<h1>Choose an Item</h1>
 	<ul id="item-list" data-world="<?= $world ?>">
 	<?php
-		$randomized = range(0, 15);
+		$ordered = range(0, 15);
 		if (!$_SESSION['ordered']) {
-			shuffle($randomized);
+			shuffle($ordered);
 		} else {
 			// paint lowest positions first, to not overlap text at bottom
-			usort($randomized, function($a, $b) use ($world, $positions) {
+			usort($ordered, function($a, $b) use ($world, $positions) {
 				if ($positions[$world][$a][0] == $positions[$world][$b][0])
 					return 0;
 				return $positions[$world][$a][0] > $positions[$world][$b][0] ? -1 : 1;
@@ -15,7 +15,7 @@
 		}
 	?>
 	<?php for ($i = 0; $i < 16; $i++): ?>
-		<?php $which = $randomized[$i]; ?>
+		<?php $which = $ordered[$i]; ?>
 		<?= component('item', compact('map', 'world', 'which', 'positions')) ?>
 	<?php endfor; ?>
 	</ul>
